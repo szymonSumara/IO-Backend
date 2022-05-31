a034e8e33d1a6990aa70afbde258dd21a48dd8c2
Szymon Sumara <szymon.sumara.zst@gmail.com>
2022-05-31 16:46:44 +0200

    v12

--- src/main/java/pl/edu/agh/niebieskiekotki/StudentEnrollmentApplication.java
+++ src/main/java/pl/edu/agh/niebieskiekotki/StudentEnrollmentApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentEnrollmentApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(StudentEnrollmentApplication.class, args);
     }
